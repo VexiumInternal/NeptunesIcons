62c874fc35a4f2f69b8ff480a83c11ddc8b37cb8
VexiumInternal <128263304+VexiumInternal@users.noreply.github.com>
2025-01-11 21:50:02 -0500

    Create icons.lua

---
+++ icons.lua
@@ -0,0 +1,15 @@
+local Icons = {
+  Home = "rbxassetid://97154352432811",
+  Toolbox = "rbxassetid://106761815787324",
+  Edit = "rbxassetid://104571990756723",
+  Open = "rbxassetid://134209340192249",
+  Builder = "rbxassetid://81843092533587",
+  Vision = "rbxassetid://140119111859796",
+  Stats = "rbxassetid://87263921169450",
+  Settings = "rbxassetid://127893354311076",
+  Binoculars = "rbxassetid://75445167851347"
+  Hoop = "rbxassetid://120122934070175",
+  SMG = "rbxassetid://133867507665599",
+}
+
+return Icons
