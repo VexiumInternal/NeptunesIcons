e527991bf00e423b51a1cc63fd598b49ff8a34ca
VexiumInternal <128263304+VexiumInternal@users.noreply.github.com>
2025-03-19 12:30:05 -0400

    Update icons.lua

--- icons.lua
+++ icons.lua
@@ -7,9 +7,21 @@ local Icons = {
   Vision = "rbxassetid://140119111859796",
   Stats = "rbxassetid://87263921169450",
   Settings = "rbxassetid://127893354311076",
-  Binoculars = "rbxassetid://75445167851347"
+  Binoculars = "rbxassetid://75445167851347",
   Hoop = "rbxassetid://120122934070175",
   SMG = "rbxassetid://133867507665599",
+  Sword = "rbxassetid://133342780735279",
+  Farm = "rbxassetid://120335077909582",
+  Eye = "rbxassetid://120335077909582",
+  Crosshair = "rbxassetid://98672881734356",
+  Notification = "rbxassetid://104063607158538",
+  Check_Mark = "rbxassetid://80814676009246",
+  Fix = "rbxassetid://83790732731258",
+  Armor = "rbxassetid://121336897342169",
+  Shield = "rbxassetid://73036466836450",
+  Arcade = "rbxassetid://90059988395975",
+  Controller = "rbxassetid://108539943689032",
+  Archer = "rbxassetid://85827952544134"
 }
 
 return Icons
